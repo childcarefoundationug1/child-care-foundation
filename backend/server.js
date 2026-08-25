ee3e72a262e42a33f954be846bed7e5907d95c7c
child care foundation <childcarefoundation@gmail.com>
2026-08-26 00:46:48 +0300

    Force SMTP connection over IPv4

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,7 @@ const mailer = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
+    family: 4,
     connectionTimeout: 10000,
     greetingTimeout: 10000,
     socketTimeout: 10000,
